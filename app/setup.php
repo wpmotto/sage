d74d36e561155554da6f7c999f4b06525bd87d79
Greg Hunt <freshbrewedweb@gmail.com>
2021-01-20 15:25:58 -0500

    No need for jquery or vendor, and support more soil functions

--- app/setup.php
+++ app/setup.php
@@ -14,8 +14,15 @@ use function Roots\asset;
  * @return void
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_script('sage/vendor.js', asset('scripts/vendor.js')->uri(), ['jquery'], null, true);
-    wp_enqueue_script('sage/app.js', asset('scripts/app.js')->uri(), ['sage/vendor.js', 'jquery'], null, true);
+    /**
+     * Only enqueue this once we have a need for
+     * a vendor file.
+     */
+    // wp_enqueue_script('sage/vendor.js', asset('scripts/vendor.js')->uri(), [], null, true);
+
+    wp_enqueue_script('sage/app.js', asset('scripts/app.js')->uri(), [
+        // 'sage/vendor.js'
+    ], null, true);
 
     wp_add_inline_script('sage/vendor.js', asset('scripts/manifest.js')->contents(), 'before');
 
@@ -53,10 +60,123 @@ add_action('after_setup_theme', function () {
      * @link https://roots.io/plugins/soil/
      */
     add_theme_support('soil', [
-        'clean-up',
+        /**
+         * Clean up WordPress
+         */
+        'clean-up' => [
+            /**
+             * Obscure and suppress WordPress information.
+             */
+            'wp_obscurity',
+
+            /**
+             * Disable WordPress emojis.
+             */
+            'disable_emojis',
+
+            /**
+             * Disable Gutenberg block library CSS.
+             */
+            'disable_gutenberg_block_css',
+
+            /**
+             * Disable extra RSS feeds.
+             */
+            'disable_extra_rss',
+
+            /**
+             * Disable recent comments CSS.
+             */
+            'disable_recent_comments_css',
+
+            /**
+             * Disable gallery CSS.
+             */
+            'disable_gallery_css',
+
+            /**
+             * Clean HTML5 markup.
+             */
+            'clean_html5_markup',
+        ],
+
+        /**
+         * Disable WordPress REST API
+         */
+        'disable-rest-api',
+
+        /**
+         * Remove version query string from all styles and scripts
+         */
+        'disable-asset-versioning',
+
+        /**
+         * Disables trackbacks/pingbacks
+         */
+        'disable-trackbacks',
+
+        /**
+         * Google Analytics
+         */
+        'google-analytics' => [
+            /**
+             * This is to go live with GA.
+             *
+             * This should probably be false in non-production.
+             */
+            'should_load' => false,
+
+            /**
+             * Google Analytics ID
+             *
+             * This is also known as your "property ID" or "measurement ID"
+             *
+             * Format: UA-XXXXX-Y
+             */
+            'google_analytics_id' => null,
+
+            /**
+             * Optimize container ID
+             *
+             * Format: OPT-A1B2CD (previously: GTM-A1B2CD)
+             *
+             * @link https://support.google.com/optimize/answer/6262084
+             */
+            'optimize_id' => null,
+
+            /**
+             * Anonymize user IP addresses.
+             *
+             * This might be required depending on region.
+             *
+             * @link https://github.com/roots/soil/pull/206
+             */
+            'anonymize_ip',
+        ],
+
+        /**
+         * Moves all scripts to wp_footer action
+         */
+        'js-to-footer',
+
+        /**
+         * Cleaner walker for wp_nav_menu()
+         */
         'nav-walker',
+
+        /**
+         * Redirects search results from /?s=query to /search/query/, converts %20 to +
+         *
+         * @link http://txfx.net/wordpress-plugins/nice-search/
+         */
         'nice-search',
-        'relative-urls'
+
+        /**
+         * Convert absolute URLs to relative URLs
+         *
+         * Inspired by {@link http://www.456bereastreet.com/archive/201010/how_to_make_wordpress_urls_root_relative/}
+         */
+        'relative-urls',
     ]);
 
     /**
--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -2,6 +2,6 @@
  * External Dependencies
  */
 
-Window.onLoad( ()=> {
+onload = (() => { 
   console.log('DOM has loaded. Do other cool stuff. <= ./resources/assets/scripts/app/js');
-})
+})()
