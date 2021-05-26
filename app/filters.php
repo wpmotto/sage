36095a63a27e6f11258720f32d925f3f2e5be389
Greg Hunt <freshbrewedweb@gmail.com>
2021-05-26 18:04:33 -0400

    Block editor reset

--- app/filters.php
+++ app/filters.php
@@ -14,3 +14,13 @@ namespace App;
 add_filter('excerpt_more', function () {
     return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 });
+
+/**
+ * Resets the base styles in the 
+ * block editor so as not to interfere
+ * with ours.
+ */
+add_filter('block_editor_settings', function ($editor_settings) {
+    unset($editor_settings['styles'][0]);
+    return $editor_settings;
+});
